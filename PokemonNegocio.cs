87513ccb5d8c86d857bcfb4501caedb12d1e6a9a
RogerLedesma <Roggerfly@gmail.com>
2022-07-15 11:26:20 -0300

    dbSqle

---
+++ PokemonNegocio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace frmPokemon
+{
+    class PokemonNegocio
+    {
+
+
+        public List<Pokemon> Listar ()
+        {   
+
+            // LISTA DE POKEMON, RECOLECTANDO 
+
+            List<Pokemon> lista = new List<Pokemon>();
+
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server =.\\SQLEXPRESS; database=POKEDEX_DB; integrated security = true ";
+                // Puedo copiar y pegar "server = FX8000-PC\\SQLEXPRESS" o bien
+                // colocar un punto para que luego la lectura sea agil.
+                
+                //comando.CommandType = System.Data.CommandType.Text;
+
+                comando.CommandText = "";
+                
+
+                
+     
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
